f5d700a10eeed71ff37b5ac6e775eaf6c32cd12c
LXD Luca <cyberlucayt@gmail.com>
2025-06-29 10:04:25 +0200

    Update serverConfig.ts

--- src/config/serverConfig.ts
+++ src/config/serverConfig.ts
@@ -11,7 +11,7 @@ export interface ServerConfig {
 // Hier kannst du den Server-Status direkt ändern
 export const defaultConfig: ServerConfig = {
   // Ändere maintenance auf true wenn der Server in Wartung ist
-  maintenance: true,
+  maintenance: false,
   
   // Ändere den serverStatus zu:
   // - 'online' für Online
